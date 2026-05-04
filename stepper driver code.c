a0468383facc4a256634389314e1cf21d5454a9d
kelshen1 <kelshenlega@gmail.com>
2026-05-04 14:26:17 +0200

    Refactor stepper motor driver variable names and logic

--- stepper driver code.c
+++ stepper driver code.c
@@ -4,38 +4,53 @@
 // Description : Stepper Motor Driver code
 //==========================================================================
 */
-int DIR=5; // Direction pin
-int PUL=6; // Pulse pin
-int ENA=19; // Enable Pin
-int REV=400; // Pulse/Rev calibré sur le Driver
-int TIME=250; // temps par tour (en millisecondes)
-int DLY=TIME*1000/REV; // (2,5 minimum !!)
+int PIN_DIR=5; // Direction pin
+int PIN_PUL=6; // Pulse pin
+int PIN_ENA=19; // Enable Pin
+int PULSE=400; // Pulse/Rev calibré sur le Driver
+int TIME=1000; // temps par tour (en millisecondes)
+int DLY=TIME*1000/PULSE; // Délai entre chaque pulse ( en microsecondes)
 
 void setup() {
- pinMode (ENA, OUTPUT);
- pinMode (DIR, OUTPUT);
- pinMode (PUL, OUTPUT);
- digitalWrite(ENA,LOW); // initialisation
+ pinMode (PIN_ENA, OUTPUT);
+ pinMode (PIN_DIR, OUTPUT);
+ pinMode (PIN_PUL, OUTPUT);
+ digitalWrite(PIN_ENA,LOW); // initialisation
  delayMicroseconds(100);
 }
 void loop() {
- digitalWrite(DIR,LOW);
+ digitalWrite(PIN_DIR,LOW);     // dans un sens
  delayMicroseconds(1000000);
- for (int i=0; i<REV+1; i++)
+ for (int i=0; i<PULSE+1; i++)
  {
- digitalWrite(PUL,HIGH);
+ digitalWrite(PIN_PUL,HIGH);
  delayMicroseconds(DLY);
- digitalWrite(PUL,LOW);
+ digitalWrite(PIN_PUL,LOW);
  delayMicroseconds(DLY);
  }
- digitalWrite(DIR,HIGH);
+ digitalWrite(PIN_DIR,HIGH);    // dans l'autre sens
  delayMicroseconds(1000000);
- for (int i=0; i<REV+1; i++)
+ for (int i=0; i<PULSE+1; i++)
  {
- digitalWrite(PUL,HIGH);
+ digitalWrite(PIN_PUL,HIGH);
  delayMicroseconds(DLY);
- digitalWrite(PUL,LOW);
+ digitalWrite(PIN_PUL,LOW);
  delayMicroseconds(DLY);
  }
 
 }
+
+/*     sequence acceleration
+
+ digitalWrite(PIN_DIR,LOW);     
+ delayMicroseconds(1000000);
+ for (int i=0; i<PULSE+1; i++)
+ {
+ digitalWrite(PIN_PUL,HIGH);
+ delayMicroseconds(DLY);
+ digitalWrite(PIN_PUL,LOW);
+ delayMicroseconds(DLY);
+ TIME -= 50
+ }
+
+*/
