fc238b687a3253d45ebd8363047b801b1da70598
kelshen1 <kelshenlega@gmail.com>
2026-05-05 13:01:09 +0200

    Refactor stepper motor driver code
    Refactor stepper motor driver code for clarity and functionality.

--- stepper driver code.c
+++ stepper driver code.c
@@ -4,53 +4,62 @@
 // Description : Stepper Motor Driver code
 //==========================================================================
 */
-int PIN_DIR=5; // Direction pin
-int PIN_PUL=6; // Pulse pin
-int PIN_ENA=19; // Enable Pin
-int PULSE=400; // Pulse/Rev calibré sur le Driver
-int TIME=1000; // temps par tour (en millisecondes)
-int DLY=TIME*1000/PULSE; // Délai entre chaque pulse ( en microsecondes)
+int PIN_DIR = 5; // Direction pin
+int PIN_PUL = 6; // Pulse pin
+int PIN_ENA = 19; // Enable Pin
+int pulse = 400; // nb de pas par tour (voir reglage sur le driver)
 
 void setup() {
+ Serial.begin(9600);
  pinMode (PIN_ENA, OUTPUT);
  pinMode (PIN_DIR, OUTPUT);
  pinMode (PIN_PUL, OUTPUT);
  digitalWrite(PIN_ENA,LOW); // initialisation
  delayMicroseconds(100);
 }
+
+
 void loop() {
- digitalWrite(PIN_DIR,LOW);     // dans un sens
- delayMicroseconds(1000000);
- for (int i=0; i<PULSE+1; i++)
- {
- digitalWrite(PIN_PUL,HIGH);
- delayMicroseconds(DLY);
- digitalWrite(PIN_PUL,LOW);
- delayMicroseconds(DLY);
- }
- digitalWrite(PIN_DIR,HIGH);    // dans l'autre sens
- delayMicroseconds(1000000);
- for (int i=0; i<PULSE+1; i++)
- {
- digitalWrite(PIN_PUL,HIGH);
- delayMicroseconds(DLY);
- digitalWrite(PIN_PUL,LOW);
- delayMicroseconds(DLY);
- }
+  
+ digitalWrite(PIN_DIR,LOW);     // sens 1
+ delayMicroseconds(1000);
+ rotation(3,1,1);
+ rotation(10,2,6);
+ rotation(5,6,6);
+ rotation(10,6,2);
+ digitalWrite(PIN_DIR,HIGH);    // sens -1
+ delayMicroseconds(1000);
+ rotation(10,1,10);
+ rotation(40,10,10);
+ rotation(20,10,1);
 
 }
 
-/*     sequence acceleration
+// fonctions
 
- digitalWrite(PIN_DIR,LOW);     
- delayMicroseconds(1000000);
- for (int i=0; i<PULSE+1; i++)
+ void rotation(int nb, int vinit, int vend)
  {
- digitalWrite(PIN_PUL,HIGH);
- delayMicroseconds(DLY);
- digitalWrite(PIN_PUL,LOW);
- delayMicroseconds(DLY);
- TIME -= 50
- }
+ 
+  float delay = 250 * (11 - vinit);
+  float stepChange = 250.0 * (vinit - vend) / (nb * pulse);
+  for (int t=0; t<nb; t++)
+  {
+   for (int i=0; i<pulse; i++) 
+   {
+   Serial.print("tour n° ");
+   Serial.print(t + 1);
+   Serial.print(" / ");
+   Serial.print(nb);
+   Serial.print(" | delay = ");
+   Serial.println(delay);
 
-*/
+   digitalWrite(PIN_PUL,HIGH);
+   delayMicroseconds(delay);
+   digitalWrite(PIN_PUL,LOW);
+   delayMicroseconds(delay); 
+   delay += stepChange; 
+   }
+  }
+ }
+ 
+
